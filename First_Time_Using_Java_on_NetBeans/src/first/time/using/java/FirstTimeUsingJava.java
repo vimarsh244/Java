fe86278fd5d96fcca02e464a3c05d870da37317f
Vimarsh Shah <vimarsh244@gmail.com>
2018-06-21 16:12:06 +0530

    Initial commit

---
+++ First_Time_Using_Java_on_NetBeans/src/first/time/using/java/FirstTimeUsingJava.java
@@ -0,0 +1,14 @@
+package first.time.using.java;
+
+/**
+ *
+ * @author vimarsh
+ */
+public class FirstTimeUsingJava {
+
+   
+    public static void main(String args[]) {
+        System.out.print("\nMy First Time Using Nebeans IDE!\n Was using BlueJ first.\t But i lked this more!");
+    }
+    
+}
