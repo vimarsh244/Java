ab45dfced94a6701b446a7caded472800aea0e24
Vimarsh Shah <vimarsh244@gmail.com>
2018-06-21 17:10:42 +0530

    update

--- First_Time_Using_Java_on_NetBeans/src/first/time/using/java/FirstTimeUsingJava.java
+++ First_Time_Using_Java_on_NetBeans/src/first/time/using/java/FirstTimeUsingJava.java
@@ -8,7 +8,8 @@ public class FirstTimeUsingJava {
 
    
     public static void main(String args[]) {
-        System.out.print("\nMy First Time Using Nebeans IDE!\n Was using BlueJ first.\t But i lked this more!");
+        System.out.print("\nMy First Time Using Nebeans IDE!\n Was using BlueJ"
+                + " first.\t But i lked this more!");
     }
     
 }
